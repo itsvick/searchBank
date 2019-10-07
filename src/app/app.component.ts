9b8843800fdc2fcd238575b665cf64a7b04ac09d
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-10-08 01:40:44 +0530

    Added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,23 @@ import { BanksSearchService } from './services/banks-search.service';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit {
     CITIES = ['BANGALORE', 'MUMBAI', 'CHENNAI'];
     city = 'BANGALORE';
     searchKey = '';
     searchResults = [];
+    loading = false;
 
-    constructor(private bankSearch: BanksSearchService) {
-
-    }
-
-    ngOnChanges(changes) {
-        console.log("Changes", changes);
-    }
+    constructor(private bankSearch: BanksSearchService) { }
 
     ngOnInit() {
         this.changeCity();
     }
 
-
     changeCity(event?) {
-        console.log("Event", event);
+        this.loading = true;
         this.bankSearch.getBanks(this.city).subscribe((response: any) => {
-            console.log("Response", response);
+            this.loading = false;
             this.searchResults = response;
         });
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +19,6 @@ import { SearchFilterPipe } from './pipes/search-filter.pipe';
     HttpClientModule,
     CommonModule,
     FormsModule,
-    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
