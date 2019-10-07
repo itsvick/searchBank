04d27f469557856f03e7c7fc66c576bfb73de8d7
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-10-07 23:50:16 +0530

    updated server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static('build'));
 
 
 app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
+    // const index = path.join(__dirname, 'build', 'index.html');
     res.sendFile(index);
   });
 
