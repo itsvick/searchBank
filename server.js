a9a9db2e7c3446d27eb00e8a544d1b59695dae66
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-10-07 23:59:06 +0530

    added project name in server.js

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const path = require('path');
 
 const app = express();
 
-// // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/<name-of-app>'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/thebanksearch'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-// });
-app.use(express.static('build'));
+res.sendFile(path.join(__dirname+'/dist/thebanksearch/index.html'));
+});
+// app.use(express.static('build'));
 
 
-app.get('*', function (req, res) {
-    // const index = path.join(__dirname, 'build', 'index.html');
-    res.sendFile(index);
-  });
+// app.get('*', function (req, res) {
+//     // const index = path.join(__dirname, 'build', 'index.html');
+//     res.sendFile(index);
+//   });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
